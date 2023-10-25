3a4b7b1d2df8d7295c759ef2756bf68216cc84a7
fadrian06 <franyeradriansanchez@gmail.com>
2023-10-24 22:48:44 -0400

    Add debug info route

--- app/routes/_app.php
+++ app/routes/_app.php
@@ -1,5 +1,12 @@
 <?php
 
 app()->get('/', function () {
-    response()->json(['message' => 'Congrats!! You\'re on Leaf API']);
+    response()->json(['message' => "Congrats!! You're on Leaf API"]);
+});
+
+app()->get('/debug', function () {
+    response()->json([
+        '$_SERVER' => $_SERVER,
+        'APP_ROOT' => dirname($_SERVER['SCRIPT_FILENAME'])
+    ]);
 });
